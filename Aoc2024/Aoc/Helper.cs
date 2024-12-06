f074b0ca3b9841735f94b274fc51492c89056586
Guillaume Desktop <robertohelper@outlook.com>
2024-12-06 23:32:52 +0100

    refactor: moved files so other projects can make use of it

--- Aoc2024/Aoc/Helper.cs
+++ Aoc2024/Aoc/Helper.cs
@@ -2,5 +2,5 @@
 
 public static class Helper
 {
-    public static string GetFilesDir() => Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "files");
+    public static string GetFilesDir() => Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.Parent.FullName, "files");
 }
